327baa66af7e413ca9869e68e8f4e95079779614
Sekh Aslam Ali <skaslam1407.gmail.com>
2025-10-07 14:55:52 +0530

    Late Static Binding

---
+++ day9.php
@@ -0,0 +1,24 @@
+<?php
+
+// Late Static Binding
+
+class A {
+    public static function who() {
+        echo __CLASS__;
+    }
+    public static function test() {
+        static::who(); // Here is the late static binding
+    }
+}
+
+class B extends A {
+    public static function who() {
+        echo __CLASS__;
+    }
+}
+
+B::test(); // Outputs "B" because of late static binding
+
+
+
+?>
