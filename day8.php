3e27bfe79fb17b9a26843228211319654b33268b
Sekh Aslam Ali <skaslam1407.gmail.com>
2025-10-07 13:28:31 +0530

    Method overriding in oops example

--- day8.php
+++ day8.php
@@ -1,7 +1,22 @@
 <?php
 
+class A{
+
+    public function display(){
+        echo "Hello World";
+    }
+}
 
 
+class B extends A{
 
+    public function display(){
+        parent::display();
+        echo " from PHP";
+    }
+}
+
+$obj = new B();
+$obj->display();
 
 ?>
