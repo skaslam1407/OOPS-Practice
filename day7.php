620bfdde176289bc0c5dc7e96b41e0f369b38871
Sekh Aslam Ali <skaslam1407.gmail.com>
2025-10-07 13:11:11 +0530

    Namespace example script

--- day7.php
+++ day7.php
@@ -1,5 +1,28 @@
 <?php
 
+namespace myproject1 ;
+class A{
+    public function display() {
+        echo "Class A display method called\n";
+    }
+}
+
+namespace myproject2 ;
+class A{
+    public function display() {
+        echo "Class B display method called\n";
+    }
+}
+
+use myproject1\A as AA;
+use myproject2\A as BB;
+
+$a = new AA();
+$a->display();
+echo "<br>";
+$b = new BB();
+$b->display()
+
 
 
 
